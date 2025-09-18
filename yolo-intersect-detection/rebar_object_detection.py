ef97d45b1d388f41db1c8a250de5c95801bb3c69
Kai <hsiaohungkai@gmail.com>
2025-09-18 15:39:53 +0800

    Remove debugging code for prune_lines_using_hough_transform

--- yolo-intersect-detection/rebar_object_detection.py
+++ yolo-intersect-detection/rebar_object_detection.py
@@ -285,7 +285,6 @@ def remove_outliers(operation, points, threshold):
 
 def prune_lines_using_hough_transform(image, pc_points, pc_direction, threshold):
     # Perform Hough Transform pruning on pc
-    # lines = []
     pruned_pc = remove_outliers(get_mode_outlier_indices, pc_points, threshold)
 
     image = cv2.imread(image)
@@ -315,20 +314,12 @@ def prune_lines_using_hough_transform(image, pc_points, pc_direction, threshold)
             if radians[i] > np.pi / 2:
                 radians[i] = norm_radian(radians[i] + np.pi, pi=np.pi)
     min_radian, max_radian = np.min(radians), np.max(radians)
-    print(pc_direction)
-    # print(radians)
-    print(angles_peaks)
-    print("min radian: ", min_radian)
-    print("max radian: ", max_radian)
 
     tolerance_degrees = 5
     tolerance_rad = tolerance_degrees * np.pi / 180
     expanded_min = min_radian - tolerance_rad
     expanded_max = max_radian + tolerance_rad
-    print("expanded min: ", expanded_min)
-    print("expanded max: ", expanded_max)
 
-    # print(pc_direction, pc_points)
     indices = []
     for i, points in enumerate(pc_points):
         point1 = np.array(points[0])
@@ -341,29 +332,7 @@ def prune_lines_using_hough_transform(image, pc_points, pc_direction, threshold)
             or expanded_min <= norm_radian(radian + np.pi, np.pi) <= expanded_max
         ):
             indices.append(i)
-    print(indices)
     pc_points = [pc_points[i] for i in range(len(pc_points)) if i in indices]
-
-    print("Detected lines (rho, theta):")
-    for rho, theta in zip(dists_peaks, angles_peaks):
-        # a = np.cos(theta)
-        # b = np.sin(theta)
-
-        # x0 = rho * a
-        # y0 = rho * b
-
-        # # Calculate line endpoints
-        # x1 = int(x0 + width * (-b))
-        # y1 = int(y0 + height * (a))
-        # x2 = int(x0 - width * (-b))
-        # y2 = int(y0 - height * (a))
-
-        # # Draw the line on top of the original image
-        # plt.plot([x1, x2], [y1, y2], '-r', linewidth=0.5, alpha=0.8)
-
-        print(f"Line: rho={rho:.2f}, theta={theta:.2f}")
-
-    # print(pc_points)
 
     return pc_points
 
@@ -409,7 +378,7 @@ def add_points_to_shapes(shapes, points, pc_direction):
     return shapes
 
 
-def get_lines(image, model_path, threshold=None) -> str:
+def get_lines(image, model_path, threshold=0) -> str:
     """
     Detects rebar intersections in an image and generates connection lines using PCA alignment.
 
@@ -523,10 +492,10 @@ def get_lines(image, model_path, threshold=None) -> str:
 
     # Perform Hough Transform pruning on pc1_points and pc2_points
     pc1_points = prune_lines_using_hough_transform(
-        image, pc1_points, pc_direction["pc1"], threshold=0
+        image, pc1_points, pc_direction["pc1"], threshold=threshold
     )
     pc2_points = prune_lines_using_hough_transform(
-        image, pc2_points, pc_direction["pc2"], threshold=0
+        image, pc2_points, pc_direction["pc2"], threshold=threshold
     )
 
     add_points_to_shapes(shapes, pc1_points, pc_direction["pc1"])
